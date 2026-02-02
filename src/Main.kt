c978fb66d61a260085b9619e01b887fa49fab45e
karthikeyan <karthikeyan76652@gmail.com>
2026-02-02 11:46:26 +0530

    coroutines support is done with IO and default

--- src/Main.kt
+++ src/Main.kt
@@ -2,47 +2,57 @@ import Memory.data
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.io.File
 
 fun main() {
-    loadData()
+    runBlocking {
+        while (true) {
+            loadData()
 
-    println("======================================")
-    //Add
-    println("1. Add Employee")
+            println("======================================")
+            //Add
+            println("1. Add Employee")
 
-    //List
-    println("2. List All the Employee")
+            //List
+            println("2. List All the Employee")
 
-    //Find
-    println("3. Find Employee by Role")
+            //Find
+            println("3. Find Employee by Role")
 
-    //Filter
-    println("4. Filter Employee by Salary")
+            //Filter
+            println("4. Filter Employee by Salary")
 
-    //Delete
-    println("5. Delete Employee by Id")
+            //Delete
+            println("5. Delete Employee by Id")
 
-    //Exit
-    println("6. Exit")
+            //Exit
+            println("6. Exit")
 
-    println("======================================")
+            println("======================================")
 
-    println("Select your work to do! by numbers from 1 - 6")
+            println("Select your work to do! by numbers from 1 - 6")
 
-    val i = readln().toIntOrNull()
+            val i = readln().toIntOrNull()
 
-    when (i) {
-        1 -> addEmployee()
-        2 -> listAllEmployee()
-        3 -> findEmployeeRole()
-        4 -> filterBySalary()
-        5 -> deleteEmployeeById()
-        6 -> return
-        else -> println("Sorry Please select one of these to continue")
+            when (i) {
+                1 -> addEmployee()
+                2 -> listAllEmployee()
+                3 -> findEmployeeRole()
+                4 -> filterBySalary()
+                5 -> deleteEmployeeById()
+                6 -> {
+                    break
+                }
+                else -> println("Sorry Please select one of these to continue")
+            }
+        }
     }
 }
 
+//Local storage
 object Memory {
     var data = mutableMapOf<Int, Employee>()
 
@@ -57,13 +67,15 @@ object Memory {
     }
 }
 
-fun saveData() {
+suspend fun saveData() = withContext(Dispatchers.IO) {
     //if we are using default values in data classes, use factory
     val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     //for collection, we must specify the type
     val type = Types.newParameterizedType(
-        MutableMap::class.java, Int::class.javaObjectType, Employee::class.java
+        MutableMap::class.java,
+        Int::class.javaObjectType,
+        Employee::class.java
     )
     //pass the type inside, adapter
     val adapter = moshi.adapter<MutableMap<Int, Employee>>(type)
@@ -74,28 +86,34 @@ fun saveData() {
     file.writeText(converter)
 }
 
-fun loadData() {
+suspend fun loadData() = withContext(Dispatchers.IO) {
     val file = File("data.json")
+
+    //if the file doesn't exist at all
     if (!file.exists()) {
         data = mutableMapOf()
-        return
+        return@withContext
     }
 
     val json = file.readText()
-    if(json.isEmpty()){
+
+    //if there is no data in file
+    if (json.isEmpty()) {
         data = mutableMapOf()
-        return
+        return@withContext
     }
 
     val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     val type = Types.newParameterizedType(
-        MutableMap::class.java, Int::class.javaObjectType, Employee::class.java
+        MutableMap::class.java,
+        Int::class.javaObjectType,
+        Employee::class.java
     )
 
     val adapter = moshi.adapter<MutableMap<Int, Employee>>(type)
 
-    Memory.data = adapter.fromJson(json) ?: mutableMapOf()
+    data = adapter.fromJson(json) ?: mutableMapOf()
 }
 
 data class Employee(
@@ -106,14 +124,15 @@ enum class Role {
     MANAGER, DEVELOPER, INTERN, NO_ROLE
 }
 
-fun addEmployee() {
+//database operation
+suspend fun addEmployee() = withContext(Dispatchers.IO) {
     //NAME
     println("Employee name?")
     val inputName = readln().trim()
     //need to cover the name in small case, and add . for initial edge cases
     if (inputName.isEmpty() || !inputName.all { it.isLetter() }) {
         println("Invalid input")
-        return main()
+        return@withContext
     }
 
     //ROLE
@@ -125,23 +144,22 @@ fun addEmployee() {
         3 -> Role.INTERN
         else -> {
             println("Invalid role")
-            return main()
+            return@withContext
         }
     }
 
     //ID
-
     println("Employee id, only in numbers")
     val id = readln().toIntOrNull() ?: run {
         println("Invalid input")
-        return main()
+        return@withContext
     }
 
     //SALARY
     println("Employee Salary")
     val salary = readln().toIntOrNull() ?: run {
         println("Invalid input")
-        return main()
+        return@withContext
     }
 
     val employee = Employee(name = inputName, id = id, salary = salary, role = roleSelected)
@@ -150,43 +168,82 @@ fun addEmployee() {
 
     saveData()
     Memory.showData()
-    main()
+    return@withContext
 }
 
-fun listAllEmployee() {
+//calculation operation
+suspend fun listAllEmployee() = withContext(Dispatchers.Default) {
     Memory.showData()
-    main()
+    return@withContext
 }
 
-fun findEmployeeRole() {
+
+//calculation operation
+suspend fun findEmployeeRole() = withContext(Dispatchers.Default) {
     println("find employee by role")
     println("1. MANAGER \n2. DEVELOPER \n3. INTERN")
-    val role = readln().toIntOrNull()
+    when (val role = readln().toIntOrNull()) {
+        1 -> {
+            val result = data.filter { it.value.role == Role.MANAGER }
+            if (result.isEmpty()) {
+                println("No Employee found on Manager Role!")
+                return@withContext
+            } else {
+                data.filter { it.value.role == Role.MANAGER }.forEach { println("Manager Role: $it") }
+            }
+        }
 
-    when (role) {
-        1 -> data.filter { it.value.role == Role.MANAGER }.forEach { println("Manager Role: $it") }
-        2 -> data.filter { it.value.role == Role.DEVELOPER }.forEach { println("Developer Role: $it") }
-        3 -> data.filter { it.value.role == Role.INTERN }.forEach { println("Intern Role: $it") }
-        else -> println("Invalid role selected")
+        2 -> {
+            val result = data.filter { it.value.role == Role.DEVELOPER }
+            if (result.isEmpty()) {
+                println("No Employee found on Developer Role!")
+                return@withContext
+            }
+            data.filter { it.value.role == Role.DEVELOPER }.forEach { println("Developer Role: $it") }
+        }
+
+        3 -> {
+            val result = data.filter { it.value.role == Role.INTERN }
+            if (result.isEmpty()) {
+                println("No Employee found on Intern Role")
+                return@withContext
+            } else {
+                data.filter { it.value.role == Role.INTERN }.forEach { println("Intern Role: $it") }
+            }
+        }
+
+        else -> {
+            println("Invalid role selected")
+            return@withContext
+        }
     }
-    main()
+    return@withContext
 }
 
-fun deleteEmployeeById() {
+
+//database operations
+suspend fun deleteEmployeeById() = withContext(Dispatchers.IO) {
     println("Enter the Employee ID, that you need to delete")
     val deleteId = readln().toIntOrNull()
 
-    if (deleteId == null || Memory.data.remove(deleteId) == null) {
-        println("Delete Employee failed")
-        main()
+    if (deleteId == null) {
+        println("Failed to delete Employee")
+        return@withContext
     } else {
-        data.remove(deleteId).also { println("Employee data deleted successfully") }
-        saveData()
-        main()
+        val result = data.filter { it.key == deleteId }
+        if (result.isEmpty()) {
+            println("No Employee Id like this found")
+            return@withContext
+        } else {
+            data.remove(deleteId).also { println("Employee Id deleted successfully") }
+            saveData()
+            return@withContext
+        }
     }
 }
 
-fun filterBySalary() {
+//calculation operation
+suspend fun filterBySalary() = withContext(Dispatchers.Default) {
     println("Select which type of filter you want:")
     println("1. Exact salary Amount \n2. Salary between min X and max Y")
     val enterAmount = readln().toIntOrNull()
@@ -196,32 +253,32 @@ fun filterBySalary() {
         2 -> filterAmountMaxMin()
         else -> {
             println("Invalid option")
-            main()
+            return@withContext
         }
     }
 }
 
-fun filterByExactAmount() {
+suspend fun filterByExactAmount() = withContext(Dispatchers.Default) {
     println("Enter the exact salary")
     val exact = readln().toIntOrNull()
     if (exact == null) {
         println("Invalid salary")
-        main()
+        return@withContext
     } else {
         val result = data.filter { it.value.salary == exact }
         if (result.isEmpty()) {
             println("No salary like this found")
-            main()
+            return@withContext
         } else {
             result.forEach {
                 println(it)
-                main()
+                return@withContext
             }
         }
     }
 }
 
-fun filterAmountMaxMin() {
+suspend fun filterAmountMaxMin() = withContext(Dispatchers.Default) {
     println("Enter the Minimum Salary")
     val min = readln().toIntOrNull()
     println("Enter the Maximum Salary")
@@ -229,15 +286,16 @@ fun filterAmountMaxMin() {
 
     if (min == null || max == null) {
         println("Invalid inputs")
-        main()
+        return@withContext
     } else {
         val result = data.filter { it.value.salary in min..max }
         if (result.isEmpty()) {
             println("No salary like this found")
+            return@withContext
         } else {
             result.forEach {
                 println(it)
-                main()
+                return@withContext
             }
         }
     }
